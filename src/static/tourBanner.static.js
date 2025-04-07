da82160a5a0ca188d8922144c9d5146880798d1f
Suleman Ahmed <48311202+Suleman3015@users.noreply.github.com>
2025-04-07 14:07:33 +0500

    Update tourBanner.static.js

--- src/static/tourBanner.static.js
+++ src/static/tourBanner.static.js
@@ -1,7 +1,7 @@
-import update1 from "../Images/postBanner/updatedTour1.jpeg"
-import update2 from "../Images/postBanner/updatedTour2.jpeg"
-import update3 from "../Images/postBanner/updatedTour3.jpeg"
-import update4 from "../Images/postBanner/updatedTour4.jpeg"
+import update1 from "../Images/updatedTour1.jpeg"
+import update2 from "../Images/updatedTour2.jpeg"
+import update3 from "../Images/updatedTour3.jpeg"
+import update4 from "../Images/updatedTour4.jpeg"
 
 
 export const TourBannerStatic = [
